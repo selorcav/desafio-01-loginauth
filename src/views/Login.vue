c8a1c8e8844c766caabf4e030320089d02a79070
Sebastián Lorca <44560684+selorcav@users.noreply.github.com>
2020-10-26 23:09:57 -0300

    f

---
+++ src/views/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+                v-model="email"
+              />
+              <small id="emailHelp" class="form-text text-muted"
+                >We'll never share your email with anyone else.</small
+              >
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+                v-model="pass"
+              />
+            </div>
+            <div class="form-group form-check">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                id="exampleCheck1"
+              />
+              <label class="form-check-label" for="exampleCheck1"
+                >Check me out</label
+              >
+            </div>
+            <button
+              type="submit"
+              class="btn btn-primary"
+              @click.prevent="login"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      pass: "",
+    };
+  },
+  methods: {
+    login() {
+      // auth logic
+      this.$router.push("home");
+    },
+  },
+};
+</script>
